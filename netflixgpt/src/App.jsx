1b64367a66588c623339700d5506e8f69b621681
yogesh.nanvate.gmail.com <yogesh.nanvate.gmail.com>
2026-05-25 22:45:31 +0530

    local setup and telwind config

---
+++ netflixgpt/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+   <h1 class="text-1xl font-bold underline">
+    Hello world!
+  </h1>
+  <h1>Helloooooo</h1>
+  </>
+  
+  )
+}
+
+export default App
